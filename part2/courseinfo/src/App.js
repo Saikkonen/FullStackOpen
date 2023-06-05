a35d29b98112b0fb113398e5ffcb8c3ed121bf5f
Aatu Saikkonen <Saikkonen@users.noreply.github.com>
2023-06-05 23:54:32 +0300

    Part 2.4

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -8,7 +8,7 @@ const Course = ({ course }) => {
 }
 
 const Header = ({ name }) => {
-  return <h1>{name}</h1>
+  return <h2>{name}</h2>
 }
 
 const Content = ({ content }) => {
@@ -30,31 +30,57 @@ const Part = ({ part, exercises }) => {
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      <h1>Web development curriculum</h1>
+      {courses.map(course =>
+      <Course key={course.id} course={course} />
+      )}
     </div>
   )
 }
