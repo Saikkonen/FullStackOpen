b7648afbaec4a91731f0eff9f69c2643ca1b216c
Aatu Saikkonen <Saikkonen@users.noreply.github.com>
2023-06-14 23:49:37 +0300

    Part 3.11

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -38,6 +38,7 @@ morgan.token('content', function getContent (req) {
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 app.get('/info', (request, response) => {
