0c7c99635edc78d3eba455122ee222b28427ab4a
Aatu Saikkonen <Saikkonen@users.noreply.github.com>
2023-07-07 23:59:05 +0300

    Part 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,37 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  if (visible) {
+    return (
+      <div style={blogStyle}>
+        <div>
+          {blog.title} {blog.author} <button onClick={() => setVisible(false)}>hide</button>
+        </div>
+        <div>
+          <a href={blog.url}>{blog.url}</a> <br />
+          likes {blog.likes} <button>like</button> <br />
+          {blog.user.name} <br />
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} <button onClick={() => setVisible(true)}>show</button>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
